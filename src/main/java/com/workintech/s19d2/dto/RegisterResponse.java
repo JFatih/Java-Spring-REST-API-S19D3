0cfcf9c18c3e7cf129224ad33e9632da25531b8f
fatih-cakmak <cakmak.35f@gmail.com>
2024-07-14 18:17:24 +0300

    All tasks done

---
+++ src/main/java/com/workintech/s19d2/dto/RegisterResponse.java
@@ -0,0 +1,6 @@
+package com.workintech.s19d2.dto;
+
+import lombok.Getter;
+
+public record RegisterResponse(String email, String message) {
+}
---
+++ src/main/java/com/workintech/s19d2/dto/RegistrationMember.java
@@ -0,0 +1,6 @@
+package com.workintech.s19d2.dto;
+
+import lombok.Getter;
+
+public record RegistrationMember(String email, String password) {
+}
